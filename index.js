49cdb7ad6d56fc36ad2bba9be369ba42fd486fef
daniolbap <chenso-dani@hotmail.com>
2025-04-25 19:31:51 -0600

    added desing and color(tomorrow fun part :))

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$("h1").text("como");
+$(document).on("click", function () {
+  // $("h1").fadeToggle();
+  //slide, slideToggle
+  //animate only works whith numeric css parameters
+  $(".green").css("background-color", "red");
+  setTimeout(function () {
+    $(".green").css("background-color", "green");
+  }, 1000);
+});
