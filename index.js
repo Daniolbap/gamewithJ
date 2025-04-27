91a873c1ea6b67bec87aed6f7951dded918ef382
daniolbap <chenso-dani@hotmail.com>
2025-04-26 19:01:32 -0600

    pushes and click animations only im sad :(

--- index.js
+++ index.js
@@ -1,9 +1,92 @@
-$(document).on("click", function () {
-  // $("h1").fadeToggle();
-  //slide, slideToggle
-  //animate only works whith numeric css parameters
-  $(".green").css("background-color", "red");
-  setTimeout(function () {
-    $(".green").css("background-color", "green");
-  }, 1000);
-});
+function greenPush() {
+  $(".green").on("click", function () {
+    $(".green").fadeOut(200);
+    $(".green").fadeIn();
+    $(".green").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".green ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
+
+function redPush() {
+  $(".red").on("click", function () {
+    $(".red").fadeOut(200);
+    $(".red").fadeIn();
+    $(".red").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".red ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
+
+function yellowPush() {
+  $(".yellow").on("click", function () {
+    $(".yellow").fadeOut(200);
+    $(".yellow").fadeIn();
+    $(".yellow").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".yellow ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
+
+function bluePush() {
+  $(".blue").on("click", function () {
+    $(".blue").fadeOut(200);
+    $(".blue").fadeIn();
+    $(".blue").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".blue ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
+
+function keys() {
+  $(document).keypress(function (event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "1":
+        $(".green").fadeOut(200);
+        $(".green").fadeIn();
+        $(".green").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".green ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "2":
+        $(".red").fadeOut(200);
+        $(".red").fadeIn();
+        $(".red").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".red ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "3":
+        $(".yellow").fadeOut(200);
+        $(".yellow").fadeIn();
+        $(".yellow").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".yellow ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "4":
+        $(".blue").fadeOut(200);
+        $(".blue").fadeIn();
+        $(".blue").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".blue ").css("border", "10px solid #000");
+        }, 500);
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+greenPush();
+redPush();
+yellowPush();
+bluePush();
+keys();
