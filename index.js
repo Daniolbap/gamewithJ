cf4e650ef54930090945fc4ab1c2a9435efe268c
daniolbap <chenso-dani@hotmail.com>
2025-04-28 20:31:12 -0600

    se hizo lo que se pudo

--- index.js
+++ index.js
@@ -1,87 +1,143 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+let audioGreen = new Audio("./sounds/green.mp3");
+let audioRed = new Audio("./sounds/red.mp3");
+let audioYellow = new Audio("./sounds/yellow.mp3");
+let audioBlue = new Audio("./sounds/blue.mp3");
+let buttonColour = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickPattern = [];
+let level = 0;
+let started = false;
+function greenPush() {
+  $(".green").on("click", function () {
+    audioGreen.play();
+    $(".green").fadeOut(200);
+    $(".green").fadeIn();
+    $(".green").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".green ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
 
-var gamePattern = [];
-var userClickedPattern = [];
+function redPush() {
+  $(".red").on("click", function () {
+    audioRed.play();
+    $(".red").fadeOut(200);
+    $(".red").fadeIn();
+    $(".red").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".red ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
 
-var started = false;
-var level = 0;
+function yellowPush() {
+  $(".yellow").on("click", function () {
+    audioYellow.play();
+    $(".yellow").fadeOut(200);
+    $(".yellow").fadeIn();
+    $(".yellow").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".yellow ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
 
-$(document).keypress(function () {
+function bluePush() {
+  $(".blue").on("click", function () {
+    audioBlue.play();
+    $(".blue").fadeOut(200);
+    $(".blue").fadeIn();
+    $(".blue").css("border", "12px solid #000");
+    setTimeout(function () {
+      $(".blue ").css("border", "10px solid #000");
+    }, 500);
+  });
+}
+
+function keys() {
+  $(document).keypress(function (event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "1":
+        audioGreen.play();
+        $(".green").fadeOut(200);
+        $(".green").fadeIn();
+        $(".green").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".green ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "2":
+        audioRed.play();
+        $(".red").fadeOut(200);
+        $(".red").fadeIn();
+        $(".red").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".red ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "3":
+        audioYellow.play();
+        $(".yellow").fadeOut(200);
+        $(".yellow").fadeIn();
+        $(".yellow").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".yellow ").css("border", "10px solid #000");
+        }, 500);
+        break;
+      case "4":
+        audioBlue.play();
+        $(".blue").fadeOut(200);
+        $(".blue").fadeIn();
+        $(".blue").css("border", "12px solid #000");
+        setTimeout(function () {
+          $(".blue ").css("border", "10px solid #000");
+        }, 500);
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+$(document).on("keypress", function () {
   if (!started) {
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
     $("h1").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.length - 1);
-});
-
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
-
-    if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSequence();
-      }, 1000);
-    }
-  } else {
-    console.log("wrong");
-
-    playSound("wrong");
-
-    $("body").addClass("game-over");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-
-    $("h1").text("Game Over, Press Any Key to Restart");
-
-    //2. Call startOver() if the user gets the sequence wrong.
-    startOver();
-  }
-}
-
 function nextSequence() {
-  userClickedPattern = [];
+  userClickPattern = [];
+
   level++;
   $("h1").text("Level " + level);
 
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
+  console.log(Math.floor(Math.random() * 4));
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColour[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  playSound(randomChosenColour);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
+$(".btn").on("click", function () {
+  let userChosenColour = this.id;
 
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
-}
+  userClickPattern.push(userChosenColour);
+  console.log(userClickPattern);
+  checkAnswer(userClickPattern.length - 1);
+});
 
-//1. Create a new function called startOver().
-function startOver() {
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
+greenPush();
+redPush();
+yellowPush();
+bluePush();
+keys();
